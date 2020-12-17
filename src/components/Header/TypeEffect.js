f9ebc8d50dcafac9abb637dcb8ebe426fbb8f0ac
pranjal <pranjalmisra2410@gmail.com>
2020-12-17 21:41:43 +0530

    Added GSAP typing effect

---
+++ src/components/Header/TypeEffect.js
@@ -0,0 +1,29 @@
+import React, { useRef, useEffect } from 'react';
+import { gsap } from 'gsap';
+import { TextPlugin } from 'gsap/TextPlugin';
+
+gsap.registerPlugin(TextPlugin);
+
+const TypeEffect = () => {
+	let text = useRef(null);
+	useEffect(() => {
+		const words = [
+			'Web Developer',
+			'Full Stack Developer',
+			'Software Engineer',
+		];
+		let masterTL = gsap.timeline({ repeat: -1 });
+		words.forEach((word) => {
+			let tl = gsap.timeline({ repeat: 1, yoyo: true, repeatDelay: 1.5 });
+			tl.to(text.current, { duration: 1.5, text: word });
+			masterTL.add(tl);
+		});
+	}, []);
+	return (
+		<span ref={text} class='heading__primary--sub'>
+			&nbsp;
+		</span>
+	);
+};
+
+export default TypeEffect;
