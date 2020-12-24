ac6336ab4575b4082bdc1bac0f8791b213634c08
pranjal <pranjalmisra2410@gmail.com>
2020-12-24 22:21:26 +0530

    Broke Header into SocialLinks and SocialScroll

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,47 +1,14 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { gsap } from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import TypeEffect from './TypeEffect';
+import React, { useState, useEffect } from 'react';
 import Wave from './Wave';
 import BrandLogo from './BrandLogo';
-import linkedIn from '../../img/linked.svg';
-import git from '../../img/github.svg';
-import insta from '../../img/insta.svg';
+import SocialScroll from './SocialScroll';
 import NavLinks from './NavLinks';
 
-gsap.registerPlugin(ScrollTrigger);
 const Header = (props) => {
-	const bounce = useRef(null);
-	const bounceSocial = useRef(null);
-
 	const [state, setState] = useState('initial');
 	useEffect(() => {
-		gsap.fromTo(
-			bounce.current,
-			{ y: -100, opacity: 0 },
-			{
-				duration: 4,
-				opacity: 1,
-				y: 0,
-				ease: 'bounce',
-			}
-		);
-		gsap.fromTo(
-			bounceSocial.current,
-			{ y: 100, opacity: 0 },
-			{
-				duration: 4,
-				opacity: 1,
-				y: 0,
-				ease: 'bounce',
-			}
-		);
 		function handleWindowMouseMove() {
-			if (window.scrollY > 10) {
-				setState('fixed');
-			} else {
-				setState('initial');
-			}
+			window.scrollY > 10 ? setState('fixed') : setState('initial');
 		}
 		window.addEventListener('scroll', handleWindowMouseMove);
 		return () => window.removeEventListener('scroll', handleWindowMouseMove);
@@ -63,54 +30,7 @@ const Header = (props) => {
 					}`}
 				>
 					<h1 className='heading__primary'>
-						<span ref={bounce} className='heading__primary--main'>
-							Pranjal Misra
-						</span>
-						<TypeEffect />
-						<div ref={bounceSocial} className='header__social--wrap'>
-							<div className='heading__primary--icons'>
-								<span className='heading__primary--icon'>
-									<a
-										target='_blank'
-										rel='noreferrer'
-										href='https://www.linkedin.com/in/PranjalMisra12'
-									>
-										<img src={linkedIn} alt='LinkedIn Profile' />
-									</a>
-								</span>
-								<span className='heading__primary--icon'>
-									<a
-										target='_blank'
-										rel='noreferrer'
-										href='https://github.com/Pranjal-dev-byte'
-									>
-										<img src={git} alt='LinkedIn Profile' />
-									</a>
-								</span>
-								<span className='heading__primary--icon'>
-									<a
-										target='_blank'
-										rel='noreferrer'
-										href='https://www.instagram.com/misra_pranjal_'
-									>
-										<img src={insta} alt='LinkedIn Profile' />
-									</a>
-								</span>
-							</div>
-							<button
-								className='btn btn-resume u-margin-medium-top'
-								id='animateSocial'
-							>
-								<a
-									target='_blank'
-									rel='noreferrer'
-									href='https://drive.google.com/file/d/1nCODWuXSJ7XEsrvIuGeJsmMMitustSSa/view?usp=sharing'
-									className='header__resume'
-								>
-									Resume
-								</a>
-							</button>
-						</div>
+						<SocialScroll />
 					</h1>
 				</div>
 				<Wave />
