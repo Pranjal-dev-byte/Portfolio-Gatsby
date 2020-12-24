a70a4fee0c3d60e4c9a2b2b23f9b3732b6903310
pranjal <pranjalmisra2410@gmail.com>
2020-12-24 22:22:13 +0530

    Refactoring Social component

---
+++ src/components/Header/SocalLinks.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import linkedIn from '../../img/linked.svg';
+import git from '../../img/github.svg';
+import insta from '../../img/insta.svg';
+
+const SocalLinks = () => {
+	return (
+		<React.Fragment>
+			<div className='heading__primary--icons'>
+				<span className='heading__primary--icon'>
+					<a
+						target='_blank'
+						rel='noreferrer'
+						href='https://www.linkedin.com/in/PranjalMisra12'
+					>
+						<img src={linkedIn} alt='LinkedIn Profile' />
+					</a>
+				</span>
+				<span className='heading__primary--icon'>
+					<a
+						target='_blank'
+						rel='noreferrer'
+						href='https://github.com/Pranjal-dev-byte'
+					>
+						<img src={git} alt='LinkedIn Profile' />
+					</a>
+				</span>
+				<span className='heading__primary--icon'>
+					<a
+						target='_blank'
+						rel='noreferrer'
+						href='https://www.instagram.com/misra_pranjal_'
+					>
+						<img src={insta} alt='LinkedIn Profile' />
+					</a>
+				</span>
+			</div>
+			<button className='btn btn-resume u-margin-medium-top' id='animateSocial'>
+				<a
+					target='_blank'
+					rel='noreferrer'
+					href='https://drive.google.com/file/d/1nCODWuXSJ7XEsrvIuGeJsmMMitustSSa/view?usp=sharing'
+					className='header__resume'
+				>
+					Resume
+				</a>
+			</button>
+		</React.Fragment>
+	);
+};
+
+export default SocalLinks;
