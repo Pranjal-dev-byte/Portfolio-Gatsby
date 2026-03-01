063c069a1efa1ebfbd7f59d1a5a2a592ae6cd36d
Pranjal Misra <pranjalmisra2410@gmail.com>
2026-03-02 01:05:31 +0530

    deps, resume update

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
 	plugins: [
 		`gatsby-plugin-react-helmet`,
 		`gatsby-plugin-sass`,
-		`gatsby-plugin-advanced-sitemap`,
+		`gatsby-plugin-sitemap`,
 		{
 			resolve: 'gatsby-plugin-robots-txt',
 			options: {
